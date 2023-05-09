3e8cff99227a5dff6d261da18514f7cca3fe0485
Subha Sarkar <106266122+Subha11111@users.noreply.github.com>
2023-05-09 08:09:55 +0530

    Add files via upload

---
+++ Sophia.py
@@ -0,0 +1,87 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices= engine.getProperty('voices') #getting details of current voice
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+       engine.say(audio)      #For now, we will write the conditions later.
+       engine.runAndWait()
+def wishme():
+        hour = int(datetime.datetime.now().hour)
+        if hour>=0 and hour<12:
+               speak("Good Morning!")
+        elif hour>=12 and hour<18:
+               speak("Good Afternoon!")
+        else:
+               speak("Good Evening!")
+
+        speak("I am Sophia. Please tell me how may I help you")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    return query
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+    # if 1:
+        query = takeCommand().lower() #Converting user query into lower case
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:  #if wikipedia found in the query then this block will be executed
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=4) 
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+            speak("Opening Youtube")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+            speak("Opening Google")
+
+        elif 'Who are you' in query:
+            print("I am Sophia an AI based voice assistant programmed by Shubho")
+            speak("I am Sophia an AI based voice assistant programmed by Shubho")
+
+        # elif 'What is your name' in query:
+        #     speak("My name is Sophia")
+        #     print("My name is Sophia")
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\home\\Desktop\\Coding\\Another Hindi Playlist\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
